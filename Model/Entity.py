322441dc1cf90162e9cd8ffdc8f41e4b227f9c9a
Thiago <thiagobesposito@gmail.com>
2024-06-23 19:52:12 -0300

    Primeiro commit. Diagrama UML
    Signed-off-by: Thiago <thiagobesposito@gmail.com>

---
+++ Model/Entity.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Entity:
+    def __init__(self):
+        pass
+
+    def name(self, ):
+        pass
+
+    def surf(self, ):
+        pass
+
+    def rect(self, ):
+        pass
+
