261f7504ddb8b868a788e24ae3db23fb3fe12e73
Thiago <thiagobesposito@gmail.com>
2024-07-19 01:09:08 -0300

    alteraçaão simples

--- code/Entity.py
+++ code/Entity.py
@@ -10,7 +10,7 @@ from code.const import ENTITY_HEALTH
 class Entity(ABC):
     def __init__(self, name: str, position: tuple):
         self.name = name
-          self.surf = pygame.image.load('asset/' + name + '.png')
+        self.surf = pygame.image.load('asset/' + name + '.png')
         #self.rect = self.surf.get_rect(topleft=position)  # Usar 'topleft' para definir a posição inicial
         self.rect = self.surf.get_rect(left=position[0], top=position[1])
         self.speed = 0
